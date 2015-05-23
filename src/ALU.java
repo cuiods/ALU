5f2d6243c0244d929f1e0442c26cd99741f79b61
cuiods <chao14@software.nju.edu.cn>
2015-05-23 15:53:58 +0800

    初始化工作区

---
+++ src/ALU.java
@@ -0,0 +1,106 @@
+
+public class ALU {
+	// 1
+	public String calculation(String formula){
+		return null;
+	}
+
+	// 2
+	public String integerRepresentation(String number, int length) {
+		return null;
+	}
+
+	// 3
+	public String floatRepresentation(String number, int sLength, int eLength){
+		return null;
+	}
+
+	// 4
+	public String ieee754(String number, int length) {
+		return null;
+	}
+
+	// 5
+	public String integerTrueValue(String operand){
+		return null;
+	}
+
+	// 6
+	public String floatTrueValue(String operand, int sLength, int eLength){
+		return null;
+	}
+
+	// 7
+	public String negation(String operand) {
+		return null;
+	}
+
+	// 8
+	public String leftShift(String operand, int n) {
+		return null;
+	}
+
+	// 9
+	public String rightAriShift(String operand, int n) {
+		return null;
+	}
+
+	// 10
+	public String rightLogShift(String operand, int n){
+		return null;
+	}
+
+	// 11
+	public String fullAdder(char x, char y, char c) {
+		return null;
+	}
+
+	// 12
+	public String claAdder(String operand1, String operand2, char c) {
+		return null;
+	}
+
+	// 13
+	public String integerAddition(String operand1, String operand2, char c, int length) {
+		return null;
+	}
+
+	// 14
+	public String integerSubtraction(String operand1, String operand2, int length) {
+		return null;
+	}
+
+	// 15
+	public String integerMultiplication(String operand1, String operand2, int length) {
+		return null;
+	}
+
+	// 16
+	public String integerDivision (String operand1, String operand2, int length) {
+		return null;
+	}
+
+	// 17
+	public String floatAddition(String operand1, String operand2, int sLength,
+			int eLength, int gLength) {
+		return null;
+	}
+
+	// 18
+	public String floatSubtraction(String operand1, String operand2,
+			int sLength, int eLength, int gLength) {
+		return null;
+	}
+
+	// 19
+	public String floatMultiplication(String operand1, String operand2, 
+			int sLength, int eLength) {
+		return null;
+	}
+	
+	//20
+	public String floatDivision(String operand1, String operand2, 
+			int sLength, int eLength) {
+		return null;
+	}
+}
